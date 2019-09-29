7a06505e217e913c6a6d5ec17042414e48fb2036
ongthanhtoan <44467058+ongthanhtoan@users.noreply.github.com>
2019-09-29 21:16:10 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,17 +10,7 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        DB::table('users')->insert([
-            'hoten' => 'Hỗ trợ',
-            'username' => 'hotro',
-            'password' => bcrypt('hotro'),
-            'quyen' => 1
-        ]);
         DB::table('chi_nhanh')->insert(
-                [
-                    'chi_nhanh_ma' => 'CN01',
-                    'chi_nhanh_ten' => 'Chi nhánh 1',
-                ],
                 [
                     'chi_nhanh_ma' => 'CN02',
                     'chi_nhanh_ten' => 'Chi nhánh 2',
