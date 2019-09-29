edf8deef14aaa1c7fc7a581c02a2fd2e44d73882
ongthanhtoan <44467058+ongthanhtoan@users.noreply.github.com>
2019-09-29 21:13:49 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,30 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+        DB::table('users')->insert([
+            'hoten' => 'Hỗ trợ',
+            'username' => 'hotro',
+            'password' => bcrypt('hotro'),
+            'quyen' => 1
+        ]);
+        DB::table('chi_nhanh')->insert(
+                [
+                    'chi_nhanh_ma' => 'CN01',
+                    'chi_nhanh_ten' => 'Chi nhánh 1',
+                ],
+                [
+                    'chi_nhanh_ma' => 'CN02',
+                    'chi_nhanh_ten' => 'Chi nhánh 2',
+                ]
+        );
     }
+
 }
